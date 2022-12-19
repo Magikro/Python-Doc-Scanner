613f6828bb82edca50fa5922684dabcac2d9c37f
Magikro <majikrow@gmail.com>
2022-12-19 16:08:32 -0600

    Finish transform

---
+++ transform.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+
+
+def order_points(pts):
+    # Initialize coordinates that will be ordered
+    # clockwise starting from the top left.
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # Top-left will have smallest sum
+    # Bottom-right will have largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # Compute difference between points
+    # Top-right will have smallest difference,
+    # bottom-left will have largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+
+def four_point_transform(image, pts):
+    # Obtain a consistent order of points, unpack
+    # them individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # Compute width of new image
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # Computer heigh of new image
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # Construct birds eye view of image with points on it
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # Compute the perspective transform matrix and then apply
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
